9bdb3fea44827e589771383526bbdd4802eb78bd
Daler Zokirov <zokirov.d@list.ru>
2020-01-01 19:24:33 +0500

    created project with react app and added files for homepage

---
+++ clothing shop/cloth-shop/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HomePage from '../src/components/homepage.component';
+
+
+
+function App() {
+  return (
+    <div>
+      < HomePage />
+    </div>
+  );
+}
+
+export default App;
