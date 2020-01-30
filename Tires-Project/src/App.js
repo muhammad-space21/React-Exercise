786e39455953eb66d1708c3c4166bc850bb77ee3
Daler Zokirov <zokirov.d@list.ru>
2020-01-30 23:24:13 +0500

    set up a project Baloon

---
+++ Tires-Project/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+import { Switch, Route } from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Im working</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Tires-Project/src/pages/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+
+const Homepage = () => (
+    <div className='homepage'>
+
+    </div>
+);
+
+export default Homepage; 
