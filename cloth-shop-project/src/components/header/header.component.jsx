3f0adcb1121b1bc5b5bb0d940b0d50a4c167bc19
Daler Zokirov <zokirov.d@list.ru>
2020-02-03 11:36:40 +0500

    Added cart-icon andd cart dropdown to my app

--- cloth-shop-project/src/components/header/header.component.jsx
+++ cloth-shop-project/src/components/header/header.component.jsx
@@ -14,7 +14,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown';
 
 
 // Functional Component // Stateless 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link  className='logo-container' to='/'>
             <Logo className='logo' />
@@ -36,13 +36,16 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon /> 
         </div>
-        <CartDropdown />
+            {        
+                hidden ? null : <CartDropdown />
+            }
     </div>
 );
 
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden} }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header); 
