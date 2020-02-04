59d798cee1e7b34aa27d4b0a5a082e1d2a1de00a
Daler Zokirov <zokirov.d@list.ru>
2020-02-04 12:38:10 +0500

    Added addItem to cart component, updated cart.utils to group items in cart

--- cloth-shop-project/src/components/collection.items/collection.items.component.jsx
+++ cloth-shop-project/src/components/collection.items/collection.items.component.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import '../collection.items/collection.items.styles.scss';
 
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart-actions'; 
 
 import CustomButton from '../custom-button/button.component';
 
 
 // Functional Component // Stateless
-const CollectionItem = ({ id, name, imageUrl, price }) => (
-    <div className='collection-item'>
-        <div className='image' style={{ backgroundImage: `url(${imageUrl})`} }> 
-            <CustomButton 
-            className='custom-button'
-            type='submit'>
-            ADD TO CART
-            </CustomButton>
-        </div>
-        
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{'$' + price}</span>
-
+const CollectionItem = ({ item, addItem}) => {
+    const { name, price, imageUrl } = item;
+    return (
+        <div className='collection-item'>
+            <div className='image' style={{ backgroundImage: `url(${imageUrl})`} }> 
+                <CustomButton onClick={() => addItem(item)} inverted>
+                ADD TO CART
+                </CustomButton>
             </div>
-    </div>
-);
+            
+                <div className='collection-footer'>
+                    <span className='name'>{name}</span>
+                    <span className='price'>{'$' + price}</span>
 
-export default CollectionItem;
+                </div>
+        </div>
+    )
+};
+
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- cloth-shop-project/src/components/custom-button/button.component.jsx
+++ cloth-shop-project/src/components/custom-button/button.component.jsx
@@ -3,8 +3,13 @@ import './button.styles.scss';
 
 
 // Functional Component // Stateless 
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
-    <button className={ `${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}{...otherProps}>
+const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
+    <button 
+    className={ ` ${ inverted ? 'inverted' : ''} 
+    ${isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
+    {...otherProps}
+    >
         {children}
     </button>
 );
